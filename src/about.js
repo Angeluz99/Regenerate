12648c3df328c89a60d601443a4fd061d62068ed
Angeluz99 <angelgldiaz@gmail.com>
2023-12-15 22:04:43 -0600

    first commit

---
+++ src/about.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const About = () => {
+  return (
+    <section>
+      {/* Add content for each section */}
+      <h2>Quienes Somos</h2>
+
+    </section>
+  );
+};
+
+export default About;
